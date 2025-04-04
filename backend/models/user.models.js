9f5b1bf277ed3d877d2a868f79b2f70b47e103d9
mukulSharma <mukulbhardwaj531@gmail.com>
2025-04-04 23:42:21 +0530

    completed userModel

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     fullName : {
@@ -26,3 +28,20 @@ const userSchema = new mongoose.Schema({
         }
     }
 })
+
+userSchema.methods.generateAuthToken = () => {
+    const token = jwt.sign({_id : this._id}, process.env.JWT_SECRET);
+    return token;
+}
+
+userSchema.methods.comparePassword = async (password) => {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.statics.hashPassword = async (password) => {
+    return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
