7999c50d831acf52d27df7cfb75e20ee58e33677
mukulSharma <mukulbhardwaj531@gmail.com>
2025-06-04 22:13:19 +0530

    login completed

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ dotenv.config();
 const cors = require('cors');
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db.js')
 const userRoute = require('./routes/user.routes.js')
 
@@ -11,6 +12,7 @@ connectToDb();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.get("/", () => {
     res.send("Hello World");
