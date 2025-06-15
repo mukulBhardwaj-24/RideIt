18053ee3b7c035ed03e9a58972e44c362cac892f
mukulSharma <mukulbhardwaj531@gmail.com>
2025-06-15 22:45:31 +0530

    captain login done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import CaptainSignup from './pages/CaptainSignup'
 import Start from './pages/Start'
 import UserProtectedWrapper from './pages/UserProtectedWrapper'
 import CaptainHome from './pages/CaptainHome'
+import CaptainProtectedWrapper from './pages/CaptainProtectedWrapper'
 
 const App = () => {
   return (
@@ -19,9 +20,9 @@ const App = () => {
         <Route path='/signup' element={<UserSignup />} />
         <Route path='/captain-login' element={<CaptainLogin />} />
         <Route path='/captain-signup' element={<CaptainSignup />} />
-        <Route path='/captain-home' element={<CaptainHome />} />
+        <Route path='/captain-home' element={<CaptainProtectedWrapper><CaptainHome /></CaptainProtectedWrapper>} />
         <Route path='/home' element={<UserProtectedWrapper> <Home /> </UserProtectedWrapper>} />
-        <Route path='/user/logout'  element={<UserProtectedWrapper> <UserLogout /> </UserProtectedWrapper>}/>
+        <Route path='/user/logout' element={<UserProtectedWrapper> <UserLogout /> </UserProtectedWrapper>} />
       </Routes>
     </div>
   )
--- frontend/src/pages/CaptainProtectedWrapper.jsx
+++ frontend/src/pages/CaptainProtectedWrapper.jsx
@@ -1,15 +1,42 @@
 import React, {Children, useContext, useEffect} from 'react'
 import { UserDataContext } from '../context/UserContext'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios';
 
 const CaptainProtectedWrapper = ({children}) => {
     const navigate = useNavigate();
+    const [loading, setLoading] = React.useState(true);
 
     const token = localStorage.getItem('token');
 
     useEffect(() => {
         if(!token) navigate('/captain-login')
     },[token, navigate]);
+
+    axios.post(`${import.meta.env.VITE_BASE_URL}/captain/profile`, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    }).then((response) => {
+        if(response.status === 200)
+        {
+            setLoading(false);
+        }
+    }
+    ).catch((error) => {
+        console.error("Error verifying captain:", error);
+        localStorage.removeItem('token');
+        navigate('/captain-login');
+    });
+
+    if(loading)
+    {
+      return (
+        <div className='flex justify-center items-center h-screen'>
+            <img className='w-16' src="https://www.svgrepo.com/show/505031/uber-driver.svg" alt="" />
+        </div>
+      )
+    }
 
   return (
     <>
