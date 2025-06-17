447d1c7c682d107f401312790ca671dc94b51d37
mukulSharma <mukulbhardwaj531@gmail.com>
2025-06-17 22:23:07 +0530

    Home page continued

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,7 +9,45 @@ const Home = () => {
         <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1619059558110-c45be64b73ae?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
       </div>
       <div className=' flex flex-col justify-end h-screen absolute top-0 w-full'>
-        
+        <div className='h-[30%] p-6 bg-white relative'>
+          <h5 ref={panelCloseRef} onClick={() => {
+            setPanelOpen(false)
+          }} className='absolute opacity-0 right-6 top-6 text-2xl'>
+            <i className="ri-arrow-down-wide-line"></i>
+          </h5>
+          <h4 className='text-2xl font-semibold'>Find a trip</h4>
+          <form className='relative py-3' onSubmit={(e) => {
+            submitHandler(e)
+          }}>
+            <div className="line absolute h-16 w-1 top-[50%] -translate-y-1/2 left-5 bg-gray-700 rounded-full"></div>
+            <input
+              onClick={() => {
+                setPanelOpen(true)
+                setActiveField('pickup')
+              }}
+              value={pickup}
+              onChange={handlePickupChange}
+              className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full'
+              type="text"
+              placeholder='Add a pick-up location'
+            />
+            <input
+              onClick={() => {
+                setPanelOpen(true)
+                setActiveField('destination')
+              }}
+              value={destination}
+              onChange={handleDestinationChange}
+              className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full  mt-3'
+              type="text"
+              placeholder='Enter your destination' />
+          </form>
+          <button
+            onClick={findTrip}
+            className='bg-black text-white px-4 py-2 rounded-lg mt-3 w-full'>
+            Find Trip
+          </button>
+        </div>
       </div>
     </div>
   )
