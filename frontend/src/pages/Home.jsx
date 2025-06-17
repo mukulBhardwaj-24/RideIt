1055a39a4fb0e984d9e676a02cce9d1e5a00bffc
mukulSharma <mukulbhardwaj531@gmail.com>
2025-06-17 22:22:30 +0530

    Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className='h-screen relative overflow-hidden'>
+      <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+      <div className='h-screen w-screen'>
+        {/* image for temporary use  */}
+        <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1619059558110-c45be64b73ae?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      </div>
+      <div className=' flex flex-col justify-end h-screen absolute top-0 w-full'>
+        
+      </div>
     </div>
   )
 }
