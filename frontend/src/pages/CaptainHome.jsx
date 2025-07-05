9856480ec2cb56691b37ef75d5baceaaf944d854
mukulSharma <mukulbhardwaj531@gmail.com>
2025-07-05 20:15:00 +0530

    ConfirmRidePopUp.jsx added

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -3,6 +3,46 @@ import { Link } from 'react-router-dom'
 import CaptainDetails from '../components/CaptainDetails'
 
 const CaptainHome = () => {
+
+  const [ridePopupPanel, setRidePopupPanel] = useState(true);
+  const [confirmRidePopupPanel, setConfirmRidePopupPanel] = useState(false);
+  const ridePopupPanelRef = useRef(null);
+  const confirmRidePopupPanelRef = useRef(null);
+
+  useGSAP(() => {
+    if (ridePopupPanel) {
+      gsap.to(ridePopupPanelRef.current, {
+        duration: 0.5,
+        translateY: '0%',
+        ease: 'power2.out',
+      });
+    }
+    else {
+      gsap.to(ridePopupPanelRef.current, {
+        duration: 0.5,
+        translateY: '100%',
+        ease: 'power2.out',
+      });
+    }
+  }, [ridePopupPanel]);
+
+  useGSAP(() => {
+    if (confirmRidePopupPanel) {
+      gsap.to(confirmRidePopupPanelRef.current, {
+        duration: 0.5,
+        translateY: '0%',
+        ease: 'power2.out',
+      });
+    }
+    else {
+      gsap.to(confirmRidePopupPanelRef.current, {
+        duration: 0.5,
+        translateY: '100%',
+        ease: 'power2.out',
+      });
+    }
+  }, [confirmRidePopupPanel]);
+
   return (
     <div className='h-screen'>
       <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
@@ -17,6 +57,18 @@ const CaptainHome = () => {
       </div>
       <div className='h-2/5 p-6'>
         <CaptainDetails />
+      </div>
+      <div ref={ridePopupPanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
+        <RidePopUp
+          setRidePopupPanel={setRidePopupPanel}
+          setConfirmRidePopupPanel={setConfirmRidePopupPanel}
+          confirmRide={confirmRide}
+        />
+      </div>
+      <div ref={confirmRidePopupPanelRef} className='fixed w-full h-screen z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
+        <ConfirmRidePopUp
+          setConfirmRidePopupPanel={setConfirmRidePopupPanel} 
+          setRidePopupPanel={setRidePopupPanel} />
       </div>
     </div>
   )
